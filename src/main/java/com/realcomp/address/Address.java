09aa45b6f80b46e559a7169e9b2ee744a22528b4
Kyle Renfro <krenfro@real-comp.com>
2012-09-20 13:43:34 +0000

    Added parsed StreetAddress to Address object for legacy (ash) purposes. Eventually, a full text search of the address line should satisfy walk-the-street searches. Alternatively, if we had a reliable address parser, this could be eliminated.
    git-svn-id: svn+ssh://dev.real-comp.com/opt/usr/share/rcsvn/address-common/trunk@5223 cb9be8fc-bb24-4f4f-8dc9-9fc556ea96d8

--- src/main/java/com/realcomp/address/Address.java
+++ src/main/java/com/realcomp/address/Address.java
@@ -11,7 +11,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Address implements Serializable {
 
-    private String address;    
+    private String address;
     private String city;
     private String state;
     private String zip;
@@ -24,6 +24,14 @@ public class Address implements Serializable {
     private LatLongAccuracy latLongAccuracy;
     private AddressType type;
     private AddressQuality quality;
+
+    /*
+     * Including the parsed address for legacy (ash) purposes.
+     * Eventually, a full text search of the address line should satisfy walk-the-street searches.
+     * Alternatively, if we had a reliable address parser, this could be eliminated.
+     */
+    private StreetAddress parsed;
+
 
     public Address(){
     }
@@ -42,8 +50,9 @@ public class Address implements Serializable {
         latLongAccuracy = copy.latLongAccuracy;
         type = copy.type;
         quality = copy.quality;
+
     }
-    
+
 
     public String getAddress() {
         return address;
@@ -68,7 +77,7 @@ public class Address implements Serializable {
     public void setType(AddressType type) {
         this.type = type;
     }
-    
+
     public String getCity() {
         return city;
     }
@@ -149,62 +158,103 @@ public class Address implements Serializable {
         this.latLongAccuracy = latLongAccuracy;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        final Address other = (Address) obj;
-        if ((this.address == null) ? (other.address != null) : !this.address.equals(other.address))
-            return false;
-        if ((this.city == null) ? (other.city != null) : !this.city.equals(other.city))
-            return false;
-        if ((this.state == null) ? (other.state != null) : !this.state.equals(other.state))
-            return false;
-        if ((this.zip == null) ? (other.zip != null) : !this.zip.equals(other.zip))
-            return false;
-        if ((this.crrt == null) ? (other.crrt != null) : !this.crrt.equals(other.crrt))
-            return false;
-        if ((this.postnetBarcode == null) ? (other.postnetBarcode != null)
-                : !this.postnetBarcode.equals(other.postnetBarcode))
-            return false;
-        if ((this.imBarcode == null) ? (other.imBarcode != null) : !this.imBarcode.equals(other.imBarcode))
-            return false;
-        if ((this.fips == null) ? (other.fips != null) : !this.fips.equals(other.fips))
-            return false;
-        if (this.latitude != other.latitude && (this.latitude == null || !this.latitude.equals(other.latitude)))
-            return false;
-        if (this.longitude != other.longitude && (this.longitude == null || !this.longitude.equals(other.longitude)))
-            return false;
-        if (this.latLongAccuracy != other.latLongAccuracy)
-            return false;
-        if (this.type != other.type)
-            return false;
-        if (this.quality != other.quality)
-            return false;
-        return true;
+    public StreetAddress getParsed(){
+        return parsed;
+    }
+
+    public void setParsed(StreetAddress parsed){
+        this.parsed = parsed;
     }
 
     @Override
-    public int hashCode() {
+    public String toString() {
+        StringAppender sa = new StringAppender(" ");
+        sa.append(address.toString());
+        sa.append(city);
+        if (city != null && state != null && !city.isEmpty() && !state.isEmpty())
+            sa.append(",", "");
+        sa.append(state);
+        sa.append(zip);
+        if (fips != null) {
+            sa.append("[fips:");
+            sa.append(fips, "");
+            sa.append("]", "");
+        }
+        return sa.toString().trim();
+    }
+
+    @Override
+    public int hashCode(){
         int hash = 7;
-        hash = 97 * hash + (this.address != null ? this.address.hashCode() : 0);
-        hash = 97 * hash + (this.city != null ? this.city.hashCode() : 0);
-        hash = 97 * hash + (this.state != null ? this.state.hashCode() : 0);
-        hash = 97 * hash + (this.zip != null ? this.zip.hashCode() : 0);
-        hash = 97 * hash + (this.crrt != null ? this.crrt.hashCode() : 0);
-        hash = 97 * hash + (this.postnetBarcode != null ? this.postnetBarcode.hashCode() : 0);
-        hash = 97 * hash + (this.imBarcode != null ? this.imBarcode.hashCode() : 0);
-        hash = 97 * hash + (this.fips != null ? this.fips.hashCode() : 0);
-        hash = 97 * hash + (this.latitude != null ? this.latitude.hashCode() : 0);
-        hash = 97 * hash + (this.longitude != null ? this.longitude.hashCode() : 0);
-        hash = 97 * hash + (this.latLongAccuracy != null ? this.latLongAccuracy.hashCode() : 0);
-        hash = 97 * hash + (this.type != null ? this.type.hashCode() : 0);
-        hash = 97 * hash + (this.quality != null ? this.quality.hashCode() : 0);
+        hash = 89 * hash + (this.address != null ? this.address.hashCode() : 0);
+        hash = 89 * hash + (this.city != null ? this.city.hashCode() : 0);
+        hash = 89 * hash + (this.state != null ? this.state.hashCode() : 0);
+        hash = 89 * hash + (this.zip != null ? this.zip.hashCode() : 0);
+        hash = 89 * hash + (this.crrt != null ? this.crrt.hashCode() : 0);
+        hash = 89 * hash + (this.postnetBarcode != null ? this.postnetBarcode.hashCode() : 0);
+        hash = 89 * hash + (this.imBarcode != null ? this.imBarcode.hashCode() : 0);
+        hash = 89 * hash + (this.fips != null ? this.fips.hashCode() : 0);
+        hash = 89 * hash + (this.latitude != null ? this.latitude.hashCode() : 0);
+        hash = 89 * hash + (this.longitude != null ? this.longitude.hashCode() : 0);
+        hash = 89 * hash + (this.latLongAccuracy != null ? this.latLongAccuracy.hashCode() : 0);
+        hash = 89 * hash + (this.type != null ? this.type.hashCode() : 0);
+        hash = 89 * hash + (this.quality != null ? this.quality.hashCode() : 0);
+        hash = 89 * hash + (this.parsed != null ? this.parsed.hashCode() : 0);
         return hash;
     }
-    
-    
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj == null){
+            return false;
+        }
+        if (getClass() != obj.getClass()){
+            return false;
+        }
+        final Address other = (Address) obj;
+        if ((this.address == null) ? (other.address != null) : !this.address.equals(other.address)){
+            return false;
+        }
+        if ((this.city == null) ? (other.city != null) : !this.city.equals(other.city)){
+            return false;
+        }
+        if ((this.state == null) ? (other.state != null) : !this.state.equals(other.state)){
+            return false;
+        }
+        if ((this.zip == null) ? (other.zip != null) : !this.zip.equals(other.zip)){
+            return false;
+        }
+        if ((this.crrt == null) ? (other.crrt != null) : !this.crrt.equals(other.crrt)){
+            return false;
+        }
+        if ((this.postnetBarcode == null) ? (other.postnetBarcode != null) : !this.postnetBarcode.equals(other.postnetBarcode)){
+            return false;
+        }
+        if ((this.imBarcode == null) ? (other.imBarcode != null) : !this.imBarcode.equals(other.imBarcode)){
+            return false;
+        }
+        if ((this.fips == null) ? (other.fips != null) : !this.fips.equals(other.fips)){
+            return false;
+        }
+        if (this.latitude != other.latitude && (this.latitude == null || !this.latitude.equals(other.latitude))){
+            return false;
+        }
+        if (this.longitude != other.longitude && (this.longitude == null || !this.longitude.equals(other.longitude))){
+            return false;
+        }
+        if (this.latLongAccuracy != other.latLongAccuracy){
+            return false;
+        }
+        if (this.type != other.type){
+            return false;
+        }
+        if (this.quality != other.quality){
+            return false;
+        }
+        if (this.parsed != other.parsed && (this.parsed == null || !this.parsed.equals(other.parsed))){
+            return false;
+        }
+        return true;
+    }
 
 }
