0aadf5fc96a70d429657e35469f76f8fd6518e0b
Kyle Renfro <krenfro@real-comp.com>
2012-12-06 20:59:49 +0000

    un-deprecated Geolocation class and improved it in the following ways.
    Removed default constructor
    Added constructor to insure that the Geolocation, once instantiated, represents a valid location.
    Added checks around LatLongAccuracy to ensure it is never null.
    
    git-svn-id: svn+ssh://dev.real-comp.com/opt/usr/share/rcsvn/address-common/trunk@5997 cb9be8fc-bb24-4f4f-8dc9-9fc556ea96d8

--- src/main/java/com/realcomp/address/Geolocation.java
+++ src/main/java/com/realcomp/address/Geolocation.java
@@ -5,21 +5,27 @@ import java.io.Serializable;
 /**
  *
  * @author BGoering
- * @deprecated
  */
 public class Geolocation implements Serializable {
 
     private double latitude;
     private double longitude;
-    private LatLongAccuracy latLongAccuracy;
+    private LatLongAccuracy accuracy;
 
-    public Geolocation(){
+    public Geolocation(double latitude, double longitude, LatLongAccuracy accuracy){
+        if (accuracy == null){
+            throw new IllegalArgumentException("accuracy is null");
+        }
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.accuracy = accuracy;
+
     }
 
     public Geolocation(Geolocation copy){
         latitude = copy.latitude;
         longitude = copy.longitude;
-        latLongAccuracy = copy.latLongAccuracy;
+        accuracy = copy.accuracy;
     }
 
     public double getLatitude() {
@@ -39,38 +45,43 @@ public class Geolocation implements Serializable {
     }
 
     public LatLongAccuracy getLatLongAccuracy() {
-        return latLongAccuracy;
+        return accuracy;
     }
 
     public void setLatLongAccuracy(LatLongAccuracy latLongAccuracy) {
-        this.latLongAccuracy = latLongAccuracy;
+        if (accuracy == null){
+            throw new IllegalArgumentException("accuracy is null");
+        }
+        this.accuracy = latLongAccuracy;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        final Geolocation other = (Geolocation) obj;
-        if (Double.doubleToLongBits(this.latitude) != Double.doubleToLongBits(other.latitude))
-            return false;
-        if (Double.doubleToLongBits(this.longitude) != Double.doubleToLongBits(other.longitude))
-            return false;
-        if (this.latLongAccuracy != other.latLongAccuracy)
-            return false;
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 29 * hash + (int) (Double.doubleToLongBits(this.latitude) ^
-                (Double.doubleToLongBits(this.latitude) >>> 32));
-        hash = 29 * hash + (int) (Double.doubleToLongBits(this.longitude) ^
-                (Double.doubleToLongBits(this.longitude) >>> 32));
-        hash = 29 * hash + (this.latLongAccuracy != null ? this.latLongAccuracy.hashCode() : 0);
+    public int hashCode(){
+        int hash = 7;
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.latitude) ^ (Double.doubleToLongBits(this.latitude) >>> 32));
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.longitude) ^ (Double.doubleToLongBits(this.longitude) >>> 32));
+        hash = 97 * hash + (this.accuracy != null ? this.accuracy.hashCode() : 0);
         return hash;
     }
 
+    @Override
+    public boolean equals(Object obj){
+        if (obj == null){
+            return false;
+        }
+        if (getClass() != obj.getClass()){
+            return false;
+        }
+        final Geolocation other = (Geolocation) obj;
+        if (Double.doubleToLongBits(this.latitude) != Double.doubleToLongBits(other.latitude)){
+            return false;
+        }
+        if (Double.doubleToLongBits(this.longitude) != Double.doubleToLongBits(other.longitude)){
+            return false;
+        }
+        if (this.accuracy != other.accuracy){
+            return false;
+        }
+        return true;
+    }
 }
