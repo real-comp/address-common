59954cef3c2ec49936ef9de16ab818ea355eea07
Kyle Renfro <krenfro@real-comp.com>
2013-01-15 23:09:40 +0000

    git-svn-id: svn+ssh://dev.real-comp.com/opt/usr/share/rcsvn/address-common/trunk@6087 cb9be8fc-bb24-4f4f-8dc9-9fc556ea96d8

--- src/test/java/com/realcomp/address/RawAddressParserTest.java
+++ src/test/java/com/realcomp/address/RawAddressParserTest.java
@@ -15,46 +15,53 @@ public class RawAddressParserTest {
     public void testExtractZip() {
 
         String addressLine = "3520 OHIO AVE, SPC #7 ,  DICKINSON, TX  77539";
-        String expected = "[3520 OHIO AVE, SPC #7, DICKINSON, TX] 77539";
-        String expectedZIP = "77539";
 
         RawAddressParser parser = new RawAddressParser();
         RawAddress rawAddress = new RawAddress();
         rawAddress.addAddress(addressLine);
         RawAddress modifiedRawAddress = parser.parse(rawAddress);
 
-        assertEquals(expected, modifiedRawAddress.toString());
-        assertEquals(expectedZIP, modifiedRawAddress.getZip());
+         assertEquals("3520 OHIO AVE", modifiedRawAddress.getAddress().get(0));
+        assertEquals("SPC #7", modifiedRawAddress.getAddress().get(1));
+        assertEquals("DICKINSON", modifiedRawAddress.getAddress().get(2));
+        assertEquals("TX", modifiedRawAddress.getAddress().get(3));
+
+        assertEquals("77539", modifiedRawAddress.getZip());
     }
 
     @Test
     public void testExtractZip4() {
 
         String addressLine = "3520 OHIO AVE, SPC #7 ,  DICKINSON, TX  77539-1234";
-        String expected = "[3520 OHIO AVE, SPC #7, DICKINSON, TX] 77539-1234";
-        String expectedZIP = "77539-1234";
 
         RawAddressParser parser = new RawAddressParser();
         RawAddress rawAddress = new RawAddress();
         rawAddress.addAddress(addressLine);
         RawAddress modifiedRawAddress = parser.parse(rawAddress);
 
-        assertEquals(expected, modifiedRawAddress.toString());
-        assertEquals(expectedZIP, modifiedRawAddress.getZip());
+        assertEquals("3520 OHIO AVE", modifiedRawAddress.getAddress().get(0));
+        assertEquals("SPC #7", modifiedRawAddress.getAddress().get(1));
+        assertEquals("DICKINSON", modifiedRawAddress.getAddress().get(2));
+        assertEquals("TX", modifiedRawAddress.getAddress().get(3));
+        assertEquals("77539-1234", modifiedRawAddress.getZip());
+
     }
 
     @Test
     public void testExtractBadZip() {
 
         String addressLine = "3520 OHIO AVE, SPC #7 ,  DICKINSON, TX  7753";
-        String expected = "[3520 OHIO AVE, SPC #7, DICKINSON, TX  7753]";
 
         RawAddressParser parser = new RawAddressParser();
         RawAddress rawAddress = new RawAddress();
         rawAddress.addAddress(addressLine);
         RawAddress modifiedRawAddress = parser.parse(rawAddress);
 
-        assertEquals(expected, modifiedRawAddress.toString());
+        assertEquals("3520 OHIO AVE", modifiedRawAddress.getAddress().get(0));
+        assertEquals("SPC #7", modifiedRawAddress.getAddress().get(1));
+        assertEquals("DICKINSON", modifiedRawAddress.getAddress().get(2));
+        assertEquals("TX  7753", modifiedRawAddress.getAddress().get(3));
+
         assertNull(modifiedRawAddress.getZip());
     }
 
